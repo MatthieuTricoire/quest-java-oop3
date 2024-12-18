708d01819d7032a4492ce939ab9b532b4d4c9ad4
matthieu <matthieu@wegrow-app.com>
2024-12-18 09:11:23 +0100

    Eagle is free!

--- Eagle.java
+++ Eagle.java
@@ -1,24 +1,74 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
-    private boolean flying;
-    private int altitude;
+  private boolean flying;
+  private int altitude;
 
-    public Eagle(String name) {
-        super(name);
-        this.flying = false;
-        this.altitude = 0;
+  public Eagle(String name) {
+    super(name);
+    this.flying = false;
+    this.altitude = 0;
+  }
+
+  public int getAltitude() {
+    return altitude;
+  }
+
+  public boolean isFlying() {
+    return flying;
+  }
+
+  @Override
+  public String sing() {
+    return "Screech!";
+  }
+
+  /**
+   * Smooth landing. Need to at 1m or less to land.
+   */
+  @Override
+  public void land() {
+    if (this.flying && this.altitude <= 1) {
+      System.out.printf("%s lands on the ground.%n", this.getName());
+    } else {
+      System.out.printf("%s is too high, it can't land.%n", this.getName());
     }
+  }
 
-    public int getAltitude() {
-        return altitude;
+  /**
+   * Take off cap'tain
+   */
+  @Override
+  public void takeOff() {
+    if (!this.flying && this.altitude == 0) {
+      System.out.printf("%s takes off in the sky.%n", this.getName());
+      this.flying = true;
     }
+  }
 
-    public boolean isFlying() {
-        return flying;
-    }
+  /**
+   * To the moon
+   */
 
-    @Override
-    public String sing() {
-        return "Screech!";
+  @Override
+  public int ascend(int meters) {
+    if (this.flying) {
+      this.altitude = Math.min(this.altitude + meters, 999);
+      System.out.printf("%s flies upwards, altitude : %d%n.", this.getName(), this.getAltitude() + meters);
     }
+    return this.altitude;
+  }
+
+  @Override
+  public int descend(int meters) {
+    if (this.flying) {
+      this.altitude = Math.max(this.altitude - meters, 0);
+      System.out.printf("%s flies upwards, altitude : %s%n.", this.getName(), this.getAltitude());
+    }
+    return this.altitude;
+  }
+
+  @Override
+  public void glide() {
+    System.out.printf("%s glide.%n", this.getName());
+  }
 }
---
+++ Fly.java
@@ -0,0 +1,14 @@
+
+public interface Fly {
+
+  void takeOff();
+
+  int descend(int meters);
+
+  int ascend(int meters);
+
+  void land();
+
+  void glide();
+
+}
